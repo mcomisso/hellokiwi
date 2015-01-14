808ece39dfde4407cf6f8b59cc7d2a4aec3ffaf5
Matteo Comisso <teomatteo89@gmail.com>
2015-01-14 13:04:48 +0100

    edit: ranging during app-life change

--- kiwi/AppDelegate.swift
+++ kiwi/AppDelegate.swift
@@ -66,6 +66,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+        
     }
 
     func applicationDidEnterBackground(application: UIApplication) {
@@ -79,11 +80,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate
 
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        locationManager!.startRangingBeaconsInRegion(beaconRegion)
         FBAppEvents.activateApp()
     }
 
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        locationManager!.stopRangingBeaconsInRegion(beaconRegion)
     }
 }
 
